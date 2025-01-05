7a2f2f501d76d922afcf2cefafbebd6985612439
Esiyemeh Idogho <155540607+Randomzo@users.noreply.github.com>
2025-01-05 20:06:02 +0000

    CPlusPlusExamPrep

---
+++ Year2Sem1Examprep.cpp
@@ -0,0 +1,70 @@
+#include <format>
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*void backBackwords(string x){
+    int i = 0;
+    i = size(x);
+    for 
+        (i >= 0; i--;cout << x[i]);
+        
+
+
+double myvalues[] = {1.2,4.5,6.4};
+void printArray() {
+    for (int i = 0; i <= 3; i++) { // Loop through the array
+        printf("%d\n", myvalues[i]); // Print each element
+    }
+}
+*/
+
+int main (){
+    
+    //backBackwords("apple");
+    printArray();
+    return{};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Movie
+{
+private:
+    string title;
+    int timeInMins;
+    string actor;
+public:
+    Movie(const string& name, int time, const string& act ):
+        title(name), timeInMins(time), actor(act) {}
+
+    void printInfo()const{
+
+        cout <<"The movies time is: " << title << endl;
+        cout <<"The movies length is: " << timeInMins << endl;
+        cout <<"The movies len is: " << actor << endl;
+    }
+    
+};
+
+int main ()
+{
+    Movie Inceptions ("Inception 1", 2005, "Lenoardo");
+    Inceptions.printInfo();
+    return {};
+}*/
+
