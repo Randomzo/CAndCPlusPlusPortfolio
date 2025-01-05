8df390e5162f3b904a48594febf784897a72ab79
Esiyemeh Idogho <155540607+Randomzo@users.noreply.github.com>
2025-01-05 20:05:09 +0000

    Exam Prep

---
+++ cProgramingYear2se1ExamPrep.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void Backbackwords (char x)
+{
+    int i = 0;
+    int i = len(x);
+    for (i>=0; i--);
+        { printf("c%", i);
+
+    }
+
+}
+
+
+
+
+int main()
+{
+    Backbackwords('drawer');
+    //forlooptest();
+   return 0;
+}
+
+
+/*void forlooptest(){
+
+    for (int i = 0;i<10; i+=2)
+     {printf("%i ", i);}
+}*/
+
+//void printSep (double num)
+/*   double num = 4.99;
+    int i = num;
+double j = num - i ;
+printf("the interger part %d",i);
+printf("The double part: %f", j);
+
+  */
